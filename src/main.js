7546866dfc266a47fe9e0a51214ceca4e07a3485
PaulAntoineGauvreau <paulantoine.gauvreau@gmail.com>
2024-04-12 08:44:54 -0400

    cdn

--- src/main.js
+++ src/main.js
@@ -1,19 +1,17 @@
 import "./style.scss";
-import { Draggable } from "gsap";
 
 function updateFontSize() {
-	let words = document.querySelectorAll("[data-js-size]");
+    let words = document.querySelectorAll("[data-js-size]");
 
-	for (let i = 0; i < words.length; i++) {
-		let container = words[i].closest("[data-js-container]");
-		let baseFontSize = words[i].dataset.jsSize;
-		if (window.innerWidth < 768) {
-			words[i].style.fontSize = `${(baseFontSize * container.offsetWidth) / 35}px`;
-		} else {
-			words[i].style.fontSize = `${(baseFontSize * container.offsetWidth) / 31}px`;
-		}
-
-	}
+    for (let i = 0; i < words.length; i++) {
+        let container = words[i].closest("[data-js-container]");
+        let baseFontSize = words[i].dataset.jsSize;
+        if (window.innerWidth < 768) {
+            words[i].style.fontSize = `${(baseFontSize * container.offsetWidth) / 35}px`;
+        } else {
+            words[i].style.fontSize = `${(baseFontSize * container.offsetWidth) / 31}px`;
+        }
+    }
 }
 
 window.addEventListener("resize", updateFontSize);
@@ -22,9 +20,9 @@ window.addEventListener("load", updateFontSize);
 const grabElements = document.querySelectorAll("[data-js-grab]");
 
 grabElements.forEach((element) => {
-	Draggable.create(element, {
-		type: "x,y",
-		bounds: ".c-flyer",
-		edgeResistance: 0.65,
-	});
+    Draggable.create(element, {
+        type: "x,y",
+        bounds: ".c-flyer",
+        edgeResistance: 0.65,
+    });
 });
